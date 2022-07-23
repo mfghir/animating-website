325ebca73e950e67b93119dae7dd7639c3bd9046
miss-f-g <fatemeghafari98@gmail.com>
2022-07-23 11:23:53 +0430

    4.create animateSlides func

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
-const hikeExp = document.querySelector(".hike-exp")
+let controller;
+let slideScene;
 
-window.addEventListener("scroll" , scrollReveal)
+function animateSlides() {
+  //Init Controller
+  controller = new ScrollMagic.Controller();
 
-function scrollReveal(){
-    console.log(hikeExp)
+  //Select some things
+  const sliders = document.querySelectorAll(".slide");
+  const nav = document.querySelector(".nav-header");
+
+  //Loop over each sllide
+  sliders.forEach((slide, index, slides) => {
+
+    const revealImg = slide.querySelector(".reveal-img");
+    const img = slide.querySelector("img");
+    const revealText = slide.querySelector(".reveal-text");
+
+    //GSAP
+    const slideTl = gsap.timeline({
+      defaults: { duration: 1, ease: "power2.inOut" },
+    });
+
+    slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
+    slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
+    slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
+    slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
+  });
+
+
 }
+
+animateSlides();
